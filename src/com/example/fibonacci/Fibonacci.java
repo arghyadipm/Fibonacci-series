2f0b77affd139bd0ce6369be2a23e39ba86f5ca0
arghyadipm <79685467+arghyadipm@users.noreply.github.com>
2021-02-28 00:40:10 +0530

    Add files via upload

---
+++ src/com/example/fibonacci/Fibonacci.java
@@ -0,0 +1,25 @@
+package com.example.fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static double fib(double n){
+        int a=0,b=1,sum;
+        if (n <= 0)
+            return a;
+        for (int i=1; i<=n; i++){
+            System.out.println(a + " ");
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+            return a;
+    }
+
+    public static void main (String[] args){
+        Scanner s = new Scanner(System.in); //For user input
+        double n = s.nextDouble();
+        fib(n);
+    }
+}
+
